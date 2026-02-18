9524724421086dfb7e760cfcb715dfd54d2222f8
Supuni Shashikala <supuni.20231868@iit.ac.lk>
2026-02-18 19:26:15 +0530

    Update the home page

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Title = ({title, subTitle, align, font}) => {
+const Title = ({title, subTitle, align, font, color}) => {
   return (
     <div className={`flex flex-col justify-center items-center text-center ${align === "left" ? "md:items-start md:text-left" : ""}`}>
 
-     <h1 className={`text-5xl md:text-6xl ${font || "font-playfair"} text-black`}>{title}</h1> 
+     <h1 className={`text-5xl md:text-6xl ${font || "font-playfair"} ${color === 'white' ? 'text-white' : 'text-black'}`}>{title}</h1> 
 
-     <p className='text-base md:text-xl text-gray-900 mt-2 max-w-174'>{subTitle}</p>
+     <p className={`text-base md:text-xl mt-2 max-w-174 ${color === 'white' ? 'text-white' : 'text-gray-900'}`}>{subTitle}</p>
     </div>
   )
 }
